af81676c3728544122d2fac0919be46ce6333a7d
Bade Boy I. Tulabing <96521210+badeboytulabing01@users.noreply.github.com>
2024-12-15 20:26:43 +0800

    main

---
+++ crud-app/src/todos/student.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { studentService } from "./student.service";
+import { CreateStudentDto } from "./create-to.dto";
+
+//controller
+//dito kinocontrol yon mga ginagawa sa create-to.dto.ts
+
+@Controller('student')
+export class StudentController{
+    constructor(private readonly studentService: studentService){}
+  
+    @Post()
+    create(@Body() dto: CreateStudentDto){
+        return this.studentService.create(dto);
+    }
+
+
+
+
+
+
+}
---
+++ crud-app/src/todos/student.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Student } from "./student.entity";
+import { Repository } from "typeorm";
+import { CreateStudentDto } from "./create-to.dto";
+
+@Injectable()
+export class studentService{
+  constructor(@InjectRepository(Student) private readonly studentRespository: Repository<Student>) 
+  {}
+
+  // create or post data 
+  async create(dto: CreateStudentDto){
+        const student = this.studentRespository.create(dto);
+
+        return await this.studentRespository.save(student);
+
+    }
+
+ //Read data
+  
+
+
+}
