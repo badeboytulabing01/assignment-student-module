ff2a39d9c93fd95b97a0a1ca5663ae0a6335d907
Bade Boy I. Tulabing <96521210+badeboytulabing01@users.noreply.github.com>
2024-12-19 08:41:48 +0800

    Update app.module.ts

--- crud-app/src/app.module.ts
+++ crud-app/src/app.module.ts
@@ -16,7 +16,7 @@ TypeOrmModule.forRootAsync({
   useFactory: (configService: ConfigService) => ({
     type: 'mysql',
     host: configService.get('DB_HOST'),
-    port: +configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_NAME'),
